8d269cc124479a22189ee302c28657903ad0bde7
hung1299 <dotaallstar1999@gmail.com>
2023-02-03 10:02:09 +0700

    minor change

--- src/utils/APIConfig.ts
+++ src/utils/APIConfig.ts
@@ -7,12 +7,9 @@ class APIConfig {
     static GET_TRAFFIC_REFERRAL_SOURCES = "/get_traffic_referral_sources";
     static GET_DATA_BY_EVENT = "/get_data_by_event";
 
-<<<<<<< HEAD
     static GET_LIST_WORKSHEET_CONVERT = "/get-list-ws-convert";
     static GET_WS_CONVERT_INFO = "/get-ws-convert-info";
-=======
     static GET_ALL_CATEGORIES = "/get-all-categories";
->>>>>>> 30569c1 (add get all categories api)
 }
 
 export default APIConfig;
